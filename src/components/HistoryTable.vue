c2d0957e1763c1c6dad483cd1bff012e42791b10
hee-develop <hee950320@yahoo.co.jp>
2021-06-19 12:33:25 +0900

    apply state store in HistoryTable

--- src/components/HistoryTable.vue
+++ src/components/HistoryTable.vue
@@ -1,8 +1,7 @@
 <template>
-  <h3>学歴・社歴</h3>
   <table-base
-    :header="header"
-    :table-data="tableData"
+    :header="historyHead"
+    :table-data="historyData"
     :columnSizeArr="columnSizeArr"
     @add-row="addRow"
     @cell-updated="cellUpdated"
@@ -10,43 +9,26 @@
 </template>
 
 <script>
-import { reactive } from '@vue/reactivity';
 import TableBase from './TableBase.vue';
+import { mapState } from 'vuex';
 
 export default {
   components: { TableBase },
 
-  data() {
-    return {
-      header: ['西暦', '月', '学歴・社歴'],
-      tableData: [
-        ['2020', '2', 'M入社'],
-        ['2020', '2', 'M入社'],
-      ],
-      columnSizeArr: ['10%', '20%', '1fr'],
-    }
-  },
+  computed: mapState({
+    historyHead: (state) => state.history.head,
+    historyData: (state) => state.history.data,
+  }),
 
   methods: {
     addRow() {
-      this.tableData = [...this.tableData, reactive(Array(this.header.length).fill(''))];
+      this.$store.commit('addHistoryRow');
     },
 
     cellUpdated(rowIndex, columnIndex, newValue) {
-      // insert is better than spread; tableData is in Proxy
-      this.tableData[rowIndex][columnIndex] = newValue;
-
-      // const tempRow = [...this.tableData[rowIndex]];
-      // tempRow[columnIndex] = newValue;
-
-      // this.tableData = [
-      //   ...this.tableData.slice(0, rowIndex),
-      //   tempRow,
-      //   ...this.tableData.slice(rowIndex + 1),
-      // ];
-
+      this.$store.commit('updateHistory', [rowIndex, columnIndex, newValue]);
       console.log('updated');
-    }
+    },
   },
 }
 </script>
