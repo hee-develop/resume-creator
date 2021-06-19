c82b83f375f244b9d17aa218e06d61a6da021bb4
hee-develop <hee950320@yahoo.co.jp>
2021-06-19 17:14:22 +0900

    add certification table component

---
+++ src/components/CertificationTable.vue
@@ -0,0 +1,43 @@
+<template>
+  <table-base
+    :header="certificationHead"
+    :table-data="certificationData"
+    :columnSizeArr="columnSizeArr"
+    @add-row="addRow"
+    @cell-updated="cellUpdated"
+  />
+</template>
+
+<script>
+import TableBase from './TableBase.vue';
+import { mapState } from 'vuex';
+
+export default {
+  components: { TableBase },
+
+  computed: mapState({
+    certificationHead: (state) => state.certification.head,
+    certificationData: (state) => state.certification.data,
+  }),
+
+  data() {
+    return {
+      columnSizeArr: ['5em', '3em', '1fr'],
+    }
+  },
+
+  methods: {
+    addRow() {
+      this.$store.commit('addCertificationRow');
+    },
+
+    cellUpdated(rowIndex, columnIndex, newValue) {
+      this.$store.commit('updateCertification', [rowIndex, columnIndex, newValue]);
+      console.log('updated');
+    },
+  },
+}
+</script>
+
+<style>
+</style>
