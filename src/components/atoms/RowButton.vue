b5ef29859daad4c38bbf1bc5e370408a16167df0
hee-develop <hee950320@yahoo.co.jp>
2021-06-19 17:14:10 +0900

    add RowButton component

---
+++ src/components/atoms/RowButton.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    class="row-button"
+    @click="buttonClicked"
+    title="１行を増やす"
+  >
+    <span class="row-button-text">{{buttonType}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: { type: String, required: true },
+  },
+  emits: ['onButtonClicked'],
+
+  computed: {
+    buttonType() {
+      switch (this.type) {
+        case 'add':
+          return '﹢';
+        case 'minus':
+          return '﹣';
+        default:
+          return '';
+      }
+    },
+  },
+
+  methods: {
+    buttonClicked() {
+      this.$emit('onButtonClicked');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.row-button {
+  width: 2em;
+  height: 2em;
+  display: inline-flex;
+  cursor: pointer;
+  border-radius: 100%;
+  background-color: #248;
+  color: #fff;
+  box-shadow: 1px 1px 1px #bbb;
+}
+
+.row-button:active {
+
+}
+
+.row-button-text {
+  margin: auto;
+  font-weight: bold;
+  font-size: 1.6em;
+  line-height: 1em;
+  user-select: none;
+}
+</style>
